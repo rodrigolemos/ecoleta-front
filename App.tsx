baf773f4b9c5ac3776bda00210586ce9567deeee
Rodrigo <rodrigolemosrl@hotmail.com>
2021-03-28 16:51:49 -0300

    code: finish map

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { StatusBar, Text } from 'react-native'
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
@@ -18,6 +18,7 @@ export default function App() {
 
   return (
     <>
+      <StatusBar />
       <Routes />
     </>
   )
